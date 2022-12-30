ca1c4cbdca06d8ef966edbd98605b06316b1317d
siffkroete <iwanp@gmx.ch>
2022-12-30 07:53:10 +0100

    header gesetzt, alle möglichen header, klappt auch

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 // const string dest_server = "127.0.0.1";
 // const string dest_server = " 0000:0000:0000:0000:0000:0000:0000:0001"; // localhost in ipv6 // Funktioniert noch nicht
 // const string dest_server = "www.wenpas.com";
-const string dest_server = "wenpas.ch";
+const string dest_server = "php-scripts.ch";
 
 
 
@@ -37,9 +37,23 @@ int client_3(int argc, char** argv)
         string new_line = "\n"; // Mit diesem String richtige Antwort von wenpas.ch bekommen
 
         // Nachricht an den Server
-        string msg_to_server = "GET / HTTP/1.1" + new_line;
+        string msg_to_server = "GET /ip/wp-example-project/index.php?mode=dev HTTP/1.1" + new_line;
         msg_to_server += "Host: " + server_addr + new_line; 
         msg_to_server += "User Agent: Internet Explorer" + new_line;
+
+        msg_to_server += "Accept: */*" + new_line;
+        msg_to_server += "Accept-Language: de,fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3" + new_line;
+        // msg_to_server += "Accept-Encoding: gzip,deflate" + new_line;
+        // msg_to_server += "Accept-Charset: utf-8;q=0.7,*;q=0.7" + new_line;
+
+        msg_to_server += "Keep-Alive: 15" + new_line;
+        msg_to_server += "Connection: keep-alive" + new_line;
+        // msg_to_server += "Content-Type: application/x-www-form-urlencoded" + new_line;
+        msg_to_server += "Content-Type: text/html" + new_line;
+        msg_to_server += "X-Requested-With: XMLHttpRequest" + new_line;
+        msg_to_server += "Referer: Bla Bla etwas <script>alert(document.cookie);</script> und SQL Injection Bla Bla" + new_line;
+        msg_to_server += "Cookie: foo=bar; lorem=ipsum;" + new_line;
+        
         msg_to_server += (new_line + new_line);
 
         cout << endl << "Client: msg an Server: " << msg_to_server;
